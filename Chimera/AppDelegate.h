7e960c23f55e028b1b52248473ca732c3b83b735
Mathieu Gardère <wam@mathieugardere.com>
2013-04-28 13:01:13 +1000

    Added background sound

--- Chimera/AppDelegate.h
+++ Chimera/AppDelegate.h
@@ -7,8 +7,9 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@interface AppDelegate : UIResponder <UIApplicationDelegate, AVAudioPlayerDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
 
--- Chimera/AppDelegate.m
+++ Chimera/AppDelegate.m
@@ -8,11 +8,25 @@
 
 #import "AppDelegate.h"
 
+@interface AppDelegate() {
+    AVAudioPlayer *myPlayer;
+}
+
+@end
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     application.applicationSupportsShakeToEdit = YES;
+    
+    NSString *backgroundSoundPath = [[NSBundle mainBundle] pathForResource:@"dans_la_jungle" ofType:@"mp3"];
+    myPlayer =[[AVAudioPlayer alloc] initWithContentsOfURL:[NSURL fileURLWithPath:backgroundSoundPath] error:nil];
+    myPlayer.delegate = self;
+    [myPlayer prepareToPlay];
+    myPlayer.numberOfLoops = -1;
+    //[myExampleSound stop];
+    
     return YES;
 }
 							
@@ -20,6 +34,7 @@
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    [myPlayer stop];
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
@@ -36,6 +51,7 @@
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    [myPlayer play];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
